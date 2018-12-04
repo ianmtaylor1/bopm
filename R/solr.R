b0bec959e3b9397bac3951025a768fce08ac31a6
Ian Taylor <ian.m.taylor1@gmail.com>
2018-12-03 19:27:47 -0700

    Added progress printing option and precompution of difficult matrix inverses.

--- R/solr.R
+++ R/solr.R
@@ -6,10 +6,12 @@
 # beta.covar - prior covariance matrix for beta (regression coefficients)
 # n.iter - number of iterations per chain
 # n.chains - number of chains to run
+# print - if TRUE, print small progress notification
 # Returns: a coda mcmc.list object with all iterations
 solr <- function(y, X, ncat=max(y),
                  beta.mean=rep(0, dim(X)[2]), beta.covar=diag(dim(X)[2]),
-                 n.iter=10000, n.chains=2
+                 n.iter=10000, n.chains=2,
+                 print=FALSE
 ) {
   # Make sure the y vector looks like we want it to
   stopifnot(y == floor(y), y >= 1, y <= ncat)
@@ -22,8 +24,18 @@ solr <- function(y, X, ncat=max(y),
   n <- length(y)
   p <- dim(X)[2]
 
+  # Pre-compute difficult matrix inverses, etc.
+  beta.covar.inv <- chol2inv(chol(beta.covar))
+  beta.fc.var <- chol2inv(chol(t(xmat) %*% xmat + beta.covar.inv))
+  beta.fc.var.chol <- chol(beta.fc.var)
+
   chain.list <- list()
   for(chain in 1:n.chains) {
+
+    if (print == TRUE) {
+      cat("Chain ",chain,": (%) ",sep="")
+    }
+
     # Initial values of parameters:
     # beta - regression coefficients
     beta <- c(rmvnorm(n=1, mu=beta.mean, Sigma=beta.covar))
@@ -41,6 +53,15 @@ solr <- function(y, X, ncat=max(y),
 
     # MCMC/Gibbs sampling
     for(rep in 1:n.iter) {
+
+      if ((print == TRUE) && (rep %% (n.iter %/% 50) == 0)) {
+        if ((rep %% (n.iter %/% 10)) == 0) {
+          cat(rep %/% (n.iter %/% 100))
+        } else {
+          cat(".")
+        }
+      }
+
       # Update latent variables
       latent <- update_latent_fc(y, xmat, beta, sep)
 
@@ -48,13 +69,20 @@ solr <- function(y, X, ncat=max(y),
       sep <- update_sep_fc(y, latent, ncat)
 
       # Update beta
-      beta <- update_beta_fc(latent, xmat, beta.mean, beta.covar)
+      beta <- update_beta_fc(latent, xmat, beta.mean, beta.covar,
+                             sigma.inv=beta.covar.inv, post.var=beta.fc.var,
+                             post.var.chol=beta.fc.var.chol)
 
       # Save the current state
       results[rep,] <- c(beta,sep)
     }
 
+    # Add this chain to the results
     chain.list[[chain]] <- as.mcmc(results)
+
+    if (print == TRUE) {
+      cat("\n")
+    }
   }
 
   return(mcmc.list(chain.list))
--- R/update_beta_fc.R
+++ R/update_beta_fc.R
@@ -1,9 +1,11 @@
 # Full conditional update of beta
-update_beta_fc <- function(z, x, mu, sigma) {
-  sigma.inv <- chol2inv(chol(sigma))
-  # The posterior variance
-  post.var <- chol2inv(chol(t(x) %*% x + sigma.inv))
+update_beta_fc <- function(z, x, mu, sigma,
+                           # Options that can be pre-computed (since they don't depend on z)
+                           sigma.inv = chol2inv(chol(sigma)), # The inverse of sigma
+                           post.var = chol2inv(chol(t(x) %*% x + sigma.inv)), # The posterior variance of beta
+                           post.var.chol = chol(post.var) # Cholesky decomposition of posterior variance
+) {
   # The posterior mean
   post.mean <- post.var %*% (t(x) %*% z + sigma.inv %*% mu)
-  return( c(rmvnorm(n=1, mu=post.mean, Sigma=post.var)) )
+  return( c(rmvnorm(n=1, mu=post.mean, Sigma=post.var, Sigma.chol=post.var.chol)) )
 }
