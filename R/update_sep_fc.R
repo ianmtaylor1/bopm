ebd8e5c219e709388be03d1cbe4005576d5d6bea
Ian Taylor <ian.m.taylor1@gmail.com>
2018-12-06 15:58:24 -0700

    Added code for non-symmetric case. UNTESTED.

--- R/update_sep_fc.R
+++ R/update_sep_fc.R
@@ -1,6 +1,7 @@
 # Full conditional update of the separators that turn latent variables into
 # ordinal response Y
 update_sep_fc <- function(y, z, ncat, scale, symmetric) {
+  # Only two categories == same separator either case
   if (ncat == 2) return(c(0))
   if (symmetric == TRUE) {
     # Because of symmetry, we only need this many distinct separators
@@ -8,7 +9,8 @@ update_sep_fc <- function(y, z, ncat, scale, symmetric) {
     sep <- rep(0, ncat - 1)
     # For each of those separators, pick a random variable between the
     # min and max possible as determined by z and y. Make sure to check
-    # both positive and negative "mirrored" categories
+    # both positive and negative "mirrored" categories.
+    # Only make positive separators, then mirror to make the negative ones.
     for (i in (ncat - nsep):(ncat - 1)) {
       lbound <- max(0, abs(z[(y >= ncat + 1 - i) & (y <= i)]))
       rbound <- min(Inf, abs(z[(y < ncat + 1 - i) | (y > i)]))
@@ -17,9 +19,14 @@ update_sep_fc <- function(y, z, ncat, scale, symmetric) {
     # Sort, to fix cases of empty categories, and mirror for symmetry
     sep <- sort(sep) - sort(sep, decreasing=TRUE)
   } else {
-
-    stop("Non-symmetric case not yet implemented.")
-
+    # Now we need all separators individually. But in the non-symmetric
+    # case the first separator is always fixed at zero
+    sep <- rep(0,ncat - 1)
+    for (i in (2:(ncat - 1))) {
+      lbound <- max(0, z[y <= i])
+      rbound <- min(Inf, z[y > i])
+      sep[i] <- rtruncexp(n=1, rate=1/scale, a=lbound, b=rbound)
+    }
   }
   return(sep)
 }
