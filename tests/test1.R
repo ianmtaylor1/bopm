6d0e28fd57f238d579936b9383ed0af557f2fdef
Ian Taylor <ian.m.taylor1@gmail.com>
2018-12-18 19:31:59 -0500

    Updated test.

--- tests/test1.R
+++ tests/test1.R
@@ -5,23 +5,29 @@ library(parallel)
 rm(list=ls())
 
 n <- 1000
-test <- 20
-
-data <- data.frame(A=rnorm(n), B=rnorm(n), C=rnorm(n))
+testn <- 2
+testreps <- 1000
+testdata <- data.frame(A=rnorm(testn), B=rnorm(testn), C=rnorm(testn))
+alldata <- rbind(
+  data.frame(A=rnorm(n), B=rnorm(n), C=rnorm(n)),
+  testdata[rep(1:testn, each=testreps), ]
+)
 
 beta <- c(-2,1,4)
 intercept <- 3
-
 cutoffs <- c(0,2,4,6)
 
-data["z"] <- intercept + c(data.matrix(data) %*% beta) + rnorm(n)
-data["y"] <- rep(1,n)
+alldata["z"] <- intercept + c(data.matrix(alldata) %*% beta) + rnorm(n)
+alldata["y"] <- rep(1,dim(alldata)[1])
 for (i in 1:length(cutoffs)) {
-  data[(data["z"] > cutoffs[i]),"y"] <- i + 1
+  alldata[(alldata["z"] > cutoffs[i]),"y"] <- i + 1
 }
 
+builddata <- alldata[1:n,]
+testdata <- alldata[(n+1):dim(alldata)[1],]
+
 cl <- makeCluster(3)
-results <- bopm.parallel(y~A+B+C, data[1:(n-test),], cluster=cl,
+results <- bopm.parallel(y~A+B+C, builddata, cluster=cl,
                          n.chains=3, n.iter=15000,
                          beta.covar=100*diag(4), threshold.scale=2,
                          burn=5000, thin=100)
@@ -31,4 +37,12 @@ summary(results)
 
 traceplot(results)
 
-round(cbind(predict(results, data[(n-test+1):n,]), data[(n-test+1):n,"y"]), digits=2)
+# Test if the probabilities are accurate
+testresults <- predict(results, testdata)
+for (i in 1:testn) {
+  cat(round(testresults[(i-1)*testreps+1,], digits=2), "\n")
+  print(round(table(testdata[((i-1)*testreps+1):(i*testreps),"y"])/testreps, digits=2))
+  cat("\n\n\n")
+}
+
+
